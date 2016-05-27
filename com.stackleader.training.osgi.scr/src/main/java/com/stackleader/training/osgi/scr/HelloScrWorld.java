09ac847fe61ab9690966c3a4efcb3fb5c579ece4
David Norris <Hillrunner2008@gmail.com>
2016-05-27 09:41:09 -0400

    completed first scr example

--- com.stackleader.training.osgi.scr/src/main/java/com/stackleader/training/osgi/scr/HelloScrWorld.java
+++ com.stackleader.training.osgi.scr/src/main/java/com/stackleader/training/osgi/scr/HelloScrWorld.java
@@ -2,12 +2,14 @@ package com.stackleader.training.osgi.scr;
 
 import aQute.bnd.annotation.component.Activate;
 import aQute.bnd.annotation.component.Component;
+import aQute.bnd.annotation.component.Deactivate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- *
- * @author dcnorris
+/*
+ * This annotation declares the class as an SCR component and specifies
+ * that it should be immediately activated once its dependencies have been
+ * satisfied.
  */
 @Component(immediate = true)
 public class HelloScrWorld {
@@ -15,12 +17,37 @@ public class HelloScrWorld {
     private static final Logger LOG = LoggerFactory.getLogger(HelloScrWorld.class);
 
     public HelloScrWorld() {
-        LOG.info("HelloScrWorld constructor called");
+        //The constructor will be called first by SCR
+        LOG.info("constructor called");
     }
 
+    /*
+     * This annotation indicates to our annotation processor that this is the
+     * method we would like to have called when our component is activated. In
+     * this instance it was optional since SCR will default to calling a void
+     * method named activate if it exist; however, if we had wanted to use a
+     * non-default name, the annotation would be necessary.
+     */
     @Activate
     public void activate() {
-        LOG.info("HelloScrWorld component activated");
+        /*
+         * The Activate method will be called after any required dependencies
+         * have been injected.
+         *
+         */
+        LOG.info("component activated");
+    }
+
+    @Deactivate
+    public void deactivate() {
+        /*
+         * The deactivate lifecycle hook will fire only when an activated
+         * component component becomes disabled, the component configuration
+         * becomes unsatisfied, or the component configuration is no longer
+         * needed.
+         *
+         */
+        LOG.info("component deactivated");
     }
 
 }
